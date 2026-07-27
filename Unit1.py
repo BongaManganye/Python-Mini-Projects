755c46a0246383b54dd44e563969203ed76b8c4d
Bonga <108957720+BongaManganye@users.noreply.github.com>
2026-07-28 00:22:32 +0200

    Concert ticket booking functionality

---
+++ Unit1.py
@@ -0,0 +1,7 @@
+#Unit 1 The Concert Ticket Booker
+
+Name = input("Please enter your name")
+
+Artist = input("Please enter the name of the band/artist you want to see")
+
+print(f"Hey {Name}! Your tickets to see {Artist} are booked successfully!")
