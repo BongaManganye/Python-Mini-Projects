e126f3e987d17545e6c80eee0f06ca06179e7d56
Bonga <108957720+BongaManganye@users.noreply.github.com>
2026-07-28 00:25:46 +0200

    Secure password hint tool

---
+++ Unit 2.py
@@ -0,0 +1,12 @@
+#Unit 2 The Secure Password Hint Tool
+
+password = input("Please enter your password")
+
+#password.strip()
+#password.index()
+
+start = input("Enter the starting number")
+End = input("Enter the ending number")
+
+print(f"Your password hint: it starts with {start.upper()} and ends with {End.upper()}")
+
