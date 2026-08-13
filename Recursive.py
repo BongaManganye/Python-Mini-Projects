3b476b96aa6f3274ce31b9df3cbd2867d7e821b2
Bonga <108957720+BongaManganye@users.noreply.github.com>
2026-08-14 01:02:49 +0200

    Add recursive dependency processing script
    This script processes a list of requirements from text files recursively and prints the full list of dependencies.

---
+++ Recursive.py
@@ -0,0 +1,30 @@
+#Recursive
+#Give a bunch of files that has list of requirement in them.
+#Process them recursively and print the resulting full list of requirments
+
+import sys
+import os 
+
+if len(sys.argv) < 2:
+    exit("Usage: {} NAME".format(sys.argv[0]))
+
+start = sys.argv[1]
+
+def get_dependencies(name):
+    print("Processing {}".format(name))
+
+    deps = set(name)
+    filename = name + ".txt"
+    if not os.path.exists(filename):
+        return deps
+
+    with open(filename) as fh:
+        for line in fh:
+            row = line.rstrip("\n")
+            deps.add(row)
+            deps.update(get_dependencies(row))
+
+    return deps
+
+dependencies = get_dependencies(start)
+print(dependencies)
