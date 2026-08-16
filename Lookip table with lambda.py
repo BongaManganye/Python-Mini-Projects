b91f8e4e30968405ce2771b8125a97b4e10c9b59
Bonga <108957720+BongaManganye@users.noreply.github.com>
2026-08-17 00:53:06 +0200

    Add lookup table with lambda functions for animals

---
+++ Lookip table with lambda.py
@@ -0,0 +1,18 @@
+#Lookip table with lambda
+
+import sys
+
+table = {
+    "cat" : lambda : print("miau"),
+    "dog" : lambda : print("Hauhau"),
+}
+
+def main():
+    if len(sys.argv) != 2:
+        exit(f"Usage: {sys.argv[0]} NAME")
+
+    animal = sys.argv[1]
+    if animal in table:
+        table[animal]()
+
+main()
