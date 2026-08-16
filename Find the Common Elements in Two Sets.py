4c27fcb701f404a4f6f8f48c166967b30f48cecc
Bonga <108957720+BongaManganye@users.noreply.github.com>
2026-08-17 01:03:26 +0200

    Add script to find common elements in two sets
    This script finds and prints the common students enrolled in both Python and Java courses.

---
+++ Find the Common Elements in Two Sets.py
@@ -0,0 +1,8 @@
+#Find the Common Elements in Two Sets
+#Given two sets of student enrolled in Python and Java, Find students taking both courses
+
+Python = {"Alice", "Bonga", "Cherry", "Jack", "MacG", "David"}
+Java = {"Alice", "Ched", "Jackie", "David"}
+
+common_students = Python & Java
+print(common_students)
