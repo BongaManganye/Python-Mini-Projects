f458ce21fc34d34ee268a046ad3ad467d1d2850e
Bonga <108957720+BongaManganye@users.noreply.github.com>
2026-08-30 00:37:18 +0200

    Fix loop to display name the correct number of times
    The loop starts from 1 and goes to num-1, which means the name will be printed one less time than intended.

---
+++ Ask the user to enter their name and a number and then display their name that number of times.py
@@ -0,0 +1,7 @@
+#Ask the user to enter their name and a number and then display their name that number of times
+
+name = input("Enter name")
+num = int(input("Enter number"))
+
+for i in range(1, num):
+    print(name)
